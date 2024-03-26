412bef132ea8b9621e60f323bae5d2c421a6b335
MD ASHIKUR RAHMAN ASHIK <md.ashikur.upwork@gmail.com>
2024-03-26 13:29:51 +0600

    Books section added

---
+++ src/components/Book/Book.jsx
@@ -0,0 +1,36 @@
+import "./Book.css";
+
+const Book = ({ book }) => {
+  const { image, tags, bookName, author, category, rating } = book;
+
+  return (
+    <div className="card card-compact bg-base-100 border p-4 font-work-sans">
+      <div className="bg-[#F3F3F3] p-8">
+        <figure>
+          <img src={image} alt="Shoes" />
+        </figure>
+      </div>
+      <ul className="flex gap-3 font-medium mt-7">
+        {tags.map((tag, index) => (
+          <li className="text-[#23BE0A]" key={index}>
+            {tag}
+          </li>
+        ))}
+      </ul>
+      <div className="flex flex-col gap-0.5 mt-4">
+        <h2 className="card-title font-playfair-display font-bold text-2xl">
+          {bookName}
+        </h2>
+        <p className="font-medium mt-3 border-b pb-8">By: {author}</p>
+        <div className="card-actions mt-4 flex justify-between">
+          <p>{category}</p>
+          <div className="flex gap-2 items-center justify-center">
+            {rating} <img src="https://svgshare.com/i/14jY.svg" alt="" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Book;
---
+++ src/components/Books/Books.jsx
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react";
+import Book from "../Book/Book";
+
+const Books = () => {
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    fetch("books.json")
+      .then((res) => res.json())
+      .then((data) => setBooks(data));
+  }, []);
+
+  return (
+    <div className="mt-24 mx-32">
+      <h2 className="font-playfair-display font-bold text-4xl text-center mb-10">
+        Books
+      </h2>
+      <div className="grid grid-cols-3 gap-6 mb-28">
+        {
+            books.map(book => <Book key={book.bookId} book={book}></Book>)
+        }
+      </div>
+    </div>
+  );
+};
+
+export default Books;
