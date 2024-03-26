9240a8b85ff622d923b480b10a830e0542f8b5f6
MD ASHIKUR RAHMAN ASHIK <md.ashikur.upwork@gmail.com>
2024-03-26 13:34:23 +0600

    Deployed to surge

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -6,7 +6,7 @@ const Banner = () => {
     <div className="min-h-[calc(100vh-200px)] rounded-3xl mt-12 bg-[#1313130d] mx-32 px-28 grid">
       <div className="flex justify-center items-center flex-col lg:flex-row-reverse">
         <img
-          src="../../../public/bannerBook.svg"
+          src="https://svgshare.com/i/14i0.svg"
           className="w-3/12 rounded-lg"
         />
         <div className="w-2/3">
