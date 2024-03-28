a49943664e1e9cc79d23ccfbfcc8c2655f421737
MD ASHIKUR RAHMAN ASHIK <md.ashikur.upwork@gmail.com>
2024-03-28 21:20:53 +0600

    Implement custom bar chart

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -4,10 +4,10 @@ import "./NavBar.css";
 const NavBar = () => {
   return (
     <header className="p-4 text-black mt-12 mx-8 lg:mx-32 font-work-sans">
-      <div className="container flex justify-between h-16 mx-auto">
+      <div className="container flex gap-10 lg:gap-0 items-center justify-center">
         <Link
           to="/"
-          className="flex items-center p-2 text-2xl lg:text-[28px] font-bold"
+          className="flex p-2 text-2xl lg:text-[28px] font-bold"
         >
           Book Vibe
         </Link>
@@ -19,72 +19,7 @@ const NavBar = () => {
             Sign up
           </button>
         </div>
-        <details className="dropdown lg:hidden">
-          <summary className="m-1 btn">open or close</summary>
-          <ul className="items-stretch flex flex-col">
-            <li className="flex justify-center">
-              <NavLink
-                to="/"
-                className={({ isActive }) =>
-                  isActive
-                    ? "border-2 flex rounded-lg border-[#23BE0A] text-[#23BE0A]"
-                    : "flex items-center px-2"
-                }
-              >
-                <span className="text-center">Home</span>
-              </NavLink>
-            </li>
-            <li className="flex">
-              <NavLink
-                to="/listed-books"
-                className={({ isActive }) =>
-                  isActive
-                    ? "border-2 flex items-center px-4 rounded-lg border-[#23BE0A] text-[#23BE0A]"
-                    : "flex items-center px-4"
-                }
-              >
-                <span className="text-center">Listed Books</span>
-              </NavLink>
-            </li>
-            <li className="flex">
-              <NavLink
-                to="/pages-to-read"
-                className={({ isActive }) =>
-                  isActive
-                    ? "border-2 flex items-center px-4 rounded-lg border-[#23BE0A] text-[#23BE0A]"
-                    : "flex items-center px-4"
-                }
-              >
-                <span className="text-center">Pages To Read</span>
-              </NavLink>
-            </li>
-            <li className="flex">
-              <NavLink
-                to="/author-spotlight"
-                className={({ isActive }) =>
-                  isActive
-                    ? "border-2 flex items-center px-4 rounded-lg border-[#23BE0A] text-[#23BE0A]"
-                    : "flex items-center px-4"
-                }
-              >
-                <span className="text-center">Author Spotlight</span>
-              </NavLink>
-            </li>
-            <li className="flex">
-              <NavLink
-                to="/book-club"
-                className={({ isActive }) =>
-                  isActive
-                    ? "border-2 flex items-center px-4 rounded-lg border-[#23BE0A] text-[#23BE0A]"
-                    : "flex items-center px-4"
-                }
-              >
-                <span className="text-center">Book Club</span>
-              </NavLink>
-            </li>
-          </ul>
-        </details>
-        <ul className="items-stretch hidden lg:flex">
+        <ul className="items-stretch hidden lg:flex text-[18px] font-semibold">
           <li className="flex">
             <NavLink
               to="/"
@@ -102,8 +37,8 @@ const NavBar = () => {
               to="/listed-books"
               className={({ isActive }) =>
                 isActive
-                  ? "border-2 flex items-center px-4 rounded-lg border-[#23BE0A] text-[#23BE0A]"
-                  : "flex items-center px-4"
+                  ? "border-2 flex items-center px-2 rounded-lg border-[#23BE0A] text-[#23BE0A]"
+                  : "flex items-center px-2"
               }
             >
               <span className="text-center">Listed Books</span>
@@ -114,8 +49,8 @@ const NavBar = () => {
               to="/pages-to-read"
               className={({ isActive }) =>
                 isActive
-                  ? "border-2 flex items-center px-4 rounded-lg border-[#23BE0A] text-[#23BE0A]"
-                  : "flex items-center px-4"
+                  ? "border-2 flex items-center px-2 rounded-lg border-[#23BE0A] text-[#23BE0A]"
+                  : "flex items-center px-2"
               }
             >
               <span className="text-center">Pages To Read</span>
@@ -126,8 +61,8 @@ const NavBar = () => {
               to="/author-spotlight"
               className={({ isActive }) =>
                 isActive
-                  ? "border-2 flex items-center px-4 rounded-lg border-[#23BE0A] text-[#23BE0A]"
-                  : "flex items-center px-4"
+                  ? "border-2 flex items-center px-2 rounded-lg border-[#23BE0A] text-[#23BE0A]"
+                  : "flex items-center px-2"
               }
             >
               <span className="text-center">Author Spotlight</span>
@@ -138,8 +73,8 @@ const NavBar = () => {
               to="/book-club"
               className={({ isActive }) =>
                 isActive
-                  ? "border-2 flex items-center px-4 rounded-lg border-[#23BE0A] text-[#23BE0A]"
-                  : "flex items-center px-4"
+                  ? "border-2 flex items-center px-2 rounded-lg border-[#23BE0A] text-[#23BE0A]"
+                  : "flex items-center px-2"
               }
             >
               <span className="text-center">Book Club</span>
--- src/components/PagesToRead/PagesToRead.jsx
+++ src/components/PagesToRead/PagesToRead.jsx
@@ -1,9 +1,63 @@
+import { useEffect, useState } from "react";
+import { getBooks } from "../../utils";
+import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid } from "recharts";
+
 const PagesToRead = () => {
-    return (
-        <div>
-            <h1>Pages To Read</h1>
-        </div>
-    );
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    const storedBooks = getBooks();
+    setBooks(storedBooks);
+  }, []);
+
+  const colors = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042", "red", "pink"];
+
+  const getPath = (x, y, width, height) => {
+    return `M${x},${y + height}C${x + width / 3},${y + height} ${
+      x + width / 2
+    },${y + height / 3}
+    ${x + width / 2}, ${y}
+    C${x + width / 2},${y + height / 3} ${x + (2 * width) / 3},${y + height} ${
+      x + width
+    }, ${y + height}
+    Z`;
+  };
+
+  const TriangleBar = (props) => {
+    const { fill, x, y, width, height } = props;
+
+    return <path d={getPath(x, y, width, height)} stroke="none" fill={fill} />;
+  };
+
+  return (
+    <div className="mx-8 lg:mx-36 flex justify-center items-center mt-10">
+      <BarChart
+        width={500}
+        height={300}
+        data={books}
+        margin={{
+          top: 20,
+          right: 30,
+          left: 20,
+          bottom: 5,
+        }}
+      >
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="bookName" />
+        <YAxis />
+        <Bar
+          dataKey="totalPages"
+          fill="#8884d8"
+          shape={<TriangleBar />}
+          label={{ position: "top" }}
+        >
+          {books.map((entry, index) => (
+            <Cell key={`cell-${index}`} fill={colors[index % 20]} />
+          ))}
+        </Bar>
+      </BarChart>
+    </div>
+  );
 };
 
 export default PagesToRead;
